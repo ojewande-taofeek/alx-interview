fe58e8a4d8a7df0d5ebafc2cbb0d59bcfa77d776
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-25 09:52:02 +0100

    signal SIGINT used for keyboardInterrupt

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,6 +4,7 @@
 """
 import re
 import sys
+import signal
 
 
 total = 0
@@ -21,17 +22,17 @@ def printer():
         print("{}: {:d}".format(key, storage[key]))
 
 
-# def handler(signum, frame):
+def handler(signum, frame):
     """
         The signal handler
     Args:
         signum (int): The signal number
         frame (Object): The stack frame object
     """
-# printer()
+    printer()
 
 
-# signal.signal(signal.SIGINT, handler)
+signal.signal(signal.SIGINT, handler)
 pattern = re.compile(
     r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
     r"\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] "
@@ -41,32 +42,28 @@ pattern = re.compile(
 )
 
 
-try:
-    for line in sys.stdin:
-        match = pattern.match(line)
-        if match:
-            status = match.group(3)
-            file_size = int(match.group(4))
-            total += file_size
-            counter += 1
-            if int(status) in status_code:
-                storage[status] = storage.get(status, 0) + 1
-            if counter % 10 == 0:
-                printer()
-        else:
-            try:
-                line_split = line.split()
-                if len(line_split) > 1:
-                    file_size = int(line_split[-1])
-                    if file_size:
-                        total += file_size
-                    status = line_split[-2]
-                    if int(status) in status_code:
-                        storage[status] = storage.get(status, 0) + 1
-            except (IndexError, TypeError, ValueError):
-                pass
-    if counter % 10 != 0 or counter == 0:
-        printer()
-except KeyboardInterrupt:
+for line in sys.stdin:
+    match = pattern.match(line)
+    if match:
+        status = match.group(3)
+        file_size = int(match.group(4))
+        total += file_size
+        counter += 1
+        if int(status) in status_code:
+            storage[status] = storage.get(status, 0) + 1
+        if counter % 10 == 0:
+            printer()
+    else:
+        try:
+            line_split = line.split()
+            if len(line_split) > 1:
+                file_size = int(line_split[-1])
+                if file_size:
+                    total += file_size
+                status = line_split[-2]
+                if int(status) in status_code:
+                    storage[status] = storage.get(status, 0) + 1
+        except (IndexError, TypeError, ValueError):
+            pass
+if counter % 10 != 0 or counter == 0:
     printer()
-    raise
