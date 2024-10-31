27a8b2ad63019a167063440f651142b6ee4e9130
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-10-31 16:40:30 +0100

    UTF-8 Validation - used the UTF-8 pattern

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,10 +12,22 @@ def validUTF8(data):
     Args:
         data (list): List of integers
     """
-    try:
-        if isinstance(data, list):
-            for val in data:
-                val.to_bytes(1, byteorder='big')
-    except OverflowError:
-        return False
-    return True
+    num_bytes = 0
+    if isinstance(data, list):
+        for val in data:
+            if num_bytes == 0:  # 1-byte Character
+                if (val >> 7) == 0:
+                    num_bytes = 0
+                elif (val >> 5) == 0b110:  # 2-bytes Character
+                    num_bytes = 1
+                elif (val >> 4) == 0b1110:  # 3-bytes Character
+                    num_bytes = 2
+                elif (val >> 3) == 0b11110:  # 4-bytes Character
+                    num_bytes = 3
+                else:                 # first val does not match any character
+                    return False
+            else:
+                if (val >> 6) != 0b10:     # Not a continuation sequence
+                    return False
+                num_bytes -= 1
+        return num_bytes == 0
